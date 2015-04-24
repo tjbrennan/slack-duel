a1a50dc7ba9c1e52fee8428a75e469f55bbafa3a
Taylor Brennan <taylor.brennan@appdirect.com>
2015-04-24 01:13:20 -0700

    implement word duel

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var WebSocket = require('ws');
 var request = require('request');
+var randomWord = require('random-word');
+var cool = require('cool-ascii-faces');
 var imageGen = require('./imageGen');
 
 var botToken = process.env.SLACK_BOT_TOKEN;
 var apiToken = process.env.SLACK_API_TOKEN;
 
-
-
-
 var trigger = /let's duel/;
+var pingInterval = 15000;
 
 function connect () {
 	request({
@@ -31,52 +31,98 @@ function sockets (e, r, b) {
 	var id = 0;
 	var ping;
 	var pong;
+	var users = {};
 
+	// game vars
+	var currentWord = null;
 
+	// store users
+	b.users.forEach(function (user) {
+		users[user.id] = user.name;
+	});
+
+	// connection established
 	ws.on('open', function () {
 		console.log('Connected.');
 	});
 
-	ws.on('hello', function (data, flags) {
-	  console.log('Initialized.');
-	});
 
+	// message receiver
 	ws.on('message', function (data, flags) {
 		console.log(data);
-
 		data = JSON.parse(data);
 
-		if (/foo/.test(data.text)) {
-			console.log('foo detected')
+		// check trigger
+		if (trigger.test(data.text)) {
+			dueling = true;
+			currentWord = randomWord();
+
+			var countDown = 3;
+			var count = countDown + 2;
+			var startText = users[data.user] + ' wants to duel!';
+			
+			// start duel countdown
+			var duel = setInterval(function () {
+				if (--count === 0) {
+					ws.send(JSON.stringify({
+						id: id++,
+						type: 'message',
+						channel: data.channel,
+						text: currentWord
+					}))
+					return clearInterval(duel);
+				}
+
+				ws.send(JSON.stringify({
+					id: id++,
+					type: 'message',
+					channel: data.channel,
+					text: count > countDown ? startText : count + ''
+				}));
+			}, 1000);
+
+		} else if (data.text && !data.reply_to && data.text.indexOf(currentWord) > -1) {
+			// win condition
 			ws.send(JSON.stringify({
 				id: id++,
 				type: 'message',
 				channel: data.channel,
-				text: 'bar'
+				text: users[data.user] + ' wins! ' + cool()
 			}));
+			currentWord = null;
 		}
 	});
 
+
+	// error
 	ws.on('error', function (data, flags) {
 		throw error;
 	});
 
+	// pong receiver
 	ws.on('pong', function (data, flags) {
 		console.log(data);
 		pong = JSON.parse(data).time;
 	});
 
+	// ping sender
 	setInterval(function () {
 		ping = Date.now();
-		if (pong && ping - pong > 10000) {
+
+		if (pong && ping - pong > 2 * pingInterval) {
 			throw 'Pong out of date';
 		}
+
 		ws.send(JSON.stringify({
 			id: id++,
 			type: 'ping',
 			time: ping
 		}));
-	}, 5000)
+	}, pingInterval)
+}
+
+function sendImage (word) {
+
 }
 
 connect();
